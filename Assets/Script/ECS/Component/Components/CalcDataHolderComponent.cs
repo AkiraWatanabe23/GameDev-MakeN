ed9eb2a0de1c0bd2b717ef70c46b0557ae2e6d2a
AkiraWatanabe23 <a0323w@gmail.com>
2024-02-10 13:26:07 +0900

    [update] 入力された計算式が成り立っているかを調べる

--- Assets/Script/ECS/Component/Components/CalcDataHolderComponent.cs
+++ Assets/Script/ECS/Component/Components/CalcDataHolderComponent.cs
@@ -2,8 +2,12 @@
 
 public class CalcDataHolderComponent : IComponent
 {
+    /// <summary> 計算式に含まれる数字 </summary>
     public int[] Numbers { get; set; }
+    /// <summary> 計算式に含まれる計算記号 </summary>
     public CalculationSymbolType[] CalcSymbols { get; set; }
+    /// <summary> 数字、計算記号を含んだ計算式 </summary>
+    public string[] FromulaArray {  get; set; }
 
     public Entity Entity { get; set; }
 }
--- Assets/Script/ECS/System/GameScene/CalculationSystem.cs
+++ Assets/Script/ECS/System/GameScene/CalculationSystem.cs
@@ -1,8 +1,10 @@
 ﻿using Constants;
+using System;
 using System.Data;
 
 public class CalculationSystem : SystemBase
 {
+    private int _calcValue = 0;
     private CalcDataHolderComponent _player = default;
 
     private readonly DataTable _dataTable = new();
@@ -12,8 +14,13 @@ public class CalculationSystem : SystemBase
         //初期値、計算記号の設定
         _player = GameState.Player;
 
+        //数字の数分の長さの配列
         _player.Numbers = new int[GameState.NumbersCount];
+        //式に入れられる計算記号の数は、「数字の数 - 1」
         _player.CalcSymbols = new CalculationSymbolType[GameState.NumbersCount - 1];
+        //式に含まれる要素：数字、計算記号、数をくくる括弧
+        //n個の数がある計算式で想定される括弧の数は最大で「2^（n - 1）個」
+        _player.FromulaArray = new string[(int)Math.Pow(2, GameState.NumbersCount - 1) + GameState.NumbersCount * 2 - 1];
 
         for (int i = 0; i < _player.CalcSymbols.Length; i++) { _player.CalcSymbols[i] = GetRandomCalculationSymbol(); }
     }
@@ -27,22 +34,37 @@ public class CalculationSystem : SystemBase
     {
         if (TryParse2Formula(out string formula)) { EditorLog.Message("式が成り立っていません"); return; }
 
-        var value = GetCalcValue(formula);
+        EditorLog.Message($"計算結果：{formula} = {_calcValue}");
     }
 
     private bool TryParse2Formula(out string formula)
     {
-        formula = "";
-        return false;
+        var line = "";
+        Array.ForEach(_player.FromulaArray, calcElement => line += calcElement);
+
+        _calcValue = GetCalcValue(line);
+        if (_calcValue <= int.MinValue)
+        {
+            formula = "";
+            return false;
+        }
+
+        formula = line;
+        return true;
     }
 
     private int GetCalcValue(string formula)
     {
         // 数式の文字列からスペースを除去
         string sanitizedFormula = formula.Replace(" ", "");
+        int result = 0;
 
-        // 数式の結果を計算する
-        int result = EvaluateExpression(sanitizedFormula);
+        try
+        {
+            // 数式の結果を計算する
+            result = EvaluateExpression(sanitizedFormula);
+        }
+        catch (Exception exception) { EditorLog.Error(exception.Message); return int.MinValue; }
 
         return result;
     }
